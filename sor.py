2ebc02c20a74663ef55343be10f7b167d423861c
woraseth <voraseth@outlook.com>
2015-08-21 07:55:07 +0700

    Update sor.py

--- sor.py
+++ sor.py
@@ -1,21 +1,21 @@
 # successive over-relaxation
 
-from matrix import *
- 
+from matrix import height
+
 def sor(m, w=1.25, x0=None, eps=1e-5, max_iteration=100):
   n  = height(m)
-  b  = column(m, n)
   x0 = [0] * n if x0 == None else x0
-  x1 = x0[:]     
-  
+  x1 = x0[:]
+
   for __ in range(max_iteration):
     for i in range(n):
-      s = sum(-m[i][j] * x1[j] for j in range(n) if i != j)  
+      s = sum(-m[i][j] * x1[j] for j in range(n) if i != j)
       x1[i] = w*(m[i][n]+s)/m[i][i] + (1-w)*x0[i]
     if all(abs(x1[i]-x0[i]) < eps for i in range(n)):
-	    return x1 
-    x0 = x1[:]             
+      return x1 
+    x0 = x1[:]
   raise ValueError('Solution does not converge')
+
 
 if __name__ == '__main__':
   m = [[4,3.2,0.5,9.2], [2.2,3,-0.3,0.9], [-3.1,-0.2,4,7]]
@@ -23,3 +23,6 @@ if __name__ == '__main__':
   
 # author: Worasait Suwannik
 # date: May 2015
+ 
+ 
+
