ac65f7288095b4d4991895875992f836e29cee5c
woraseth <voraseth@outlook.com>
2015-08-24 12:40:09 +0700

    Update newton_for_sys_non_lin.py

--- newton_for_sys_non_lin.py
+++ newton_for_sys_non_lin.py
@@ -15,54 +15,37 @@ def newton_nonlinear(f, j, x0, eps=1e-5, max_iteration=100):
   ValueError : if solution doesn't converge
   """
   for __ in range(max_iteration):
+    # solve for dx
     m = [[fn(x0) for fn in j[i]] for i in range(len(j))]
     for i in range(len(f)):
       m[i].append(-f[i](x0))
-      
     import gauss
     dx = gauss.gaussian_elimination(m)
+    #import jacobi
+    #dx = jacobi.jacobi(m, [0.1] * len(x0))
     #import gauss_seidel 
-    #y = gauss_seidel.gauss_seidel(m, [1,1,1,1])
-    
+    #y = gauss_seidel.gauss_seidel(m, [-0.1] * len(x0))
+    # update x0
+    x0 = [x0[i] + dx[i] for i in range(len(dx))]
+    # terminate
     if all(i < eps for i in dx): 
       return x0
-      
-    x0 = [x0[i] + dx[i] for i in range(len(dx))]
   raise ValueError('Solution does not converge')
 
 if __name__ == '__main__':
   
-  from math import (sin, cos, e)
-  
-  # problem from aj Montri
-  f = [
-    lambda x : x[1]**2 - sin(x[0]*x[2]) - 4,
-    lambda x : x[0]**3 - 4*(x[1]+x[2])**2 + cos(x[2]) + 2,
-    lambda x : e**(x[0]+x[1]) - 5*x[1] + x[2]**2 - 4
-  ]
-  
-  j = [
-    [
-      lambda x : -x[2]*cos(x[0]*x[2]),
-      lambda x : 2*x[1],
-      lambda x : -x[0]*cos(x[0]*x[2])
-    ],
-    [
-      lambda x : 3*x[0]**2,
-      lambda x : -8*(x[1]+x[2]),
-      lambda x : -8*(x[1]+x[2])-sin(x[2])
-    ],
-    [
-      lambda x : e**(x[0]+x[1]),
-      lambda x : e**(x[0]+x[1])-5,
-      lambda x : 2*x[2]
-    ]
-  ]
-  
-  print(newton(f, j, [0, 2, -3]))
+  f = [lambda x : x[0]**2 + x[1]**2 - 17, 
+       lambda x : 2*x[0]**(1/3) + x[1]*0.5 - 4]
+  j = [[lambda x : 2*x[0], lambda x : 2*x[1]],
+       [lambda x : (2/3)*x[0]**(-2/3), lambda x : 0.5*x[1]**-0.5]]
+  x0 = [2, 3]
+  x = newton_nonlinear(f, j, [1, 2])
+  print('solution is ', x)
+  for i in range(len(f)):
+    print('f[%d]=%f' % (i, f[i](x)))
   
   
-  """
+  '''
   from math import *
   
   # problem from Burden 7ed
@@ -89,9 +72,13 @@ if __name__ == '__main__':
       lambda x : 20
     ]
   ]
+  x0 = [0.1, 0.1, -0.1]
+  x = newton_nonlinear(f, j, x0)
+  print('solution is ', x)
+  for i in range(len(f)):
+    print('f[%d]=%f' % (i, f[i](x)))
+  '''
   
-  print(newton(f, j, [0.1, 0.1, -0.1]))
-  """
 
 # newton's method for solving system of nonlinear equation
 # author : worasait suwannik
