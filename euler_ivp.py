3a12971630d46c92c08efc3af93991c30d7f0bc3
woraseth <voraseth@outlook.com>
2015-08-24 19:35:22 +0700

    IVP with x, y

--- euler_ivp.py
+++ euler_ivp.py
@@ -10,7 +10,7 @@ def euler(f, x0, y0, h, x):
       
   Returns
   -------  
-  float : f at x
+  float : solution at x
   """
   while x0 < x:
     y0 += f(x0, y0) * h
@@ -18,8 +18,11 @@ def euler(f, x0, y0, h, x):
   return y0
 
 if __name__ == '__main__':
-  f = lambda x, y : -2*x**3 + 12*x**2 - 20*x + 10
-  print(euler(f, 0, 1, 0.5, 4))
+  f = lambda x, y : (y**2 - x**2) / 5  # from http://howellkb.uah.edu//DEtext/Part2/Euler_Method.pdf
+  print(euler(f, 0, 1, 0.5, 3))
+  
+  #f = lambda x, y : -2*x**3 + 12*x**2 - 20*x + 10
+  #print(euler(f, 0, 1, 0.5, 4))
   
 # author : worasait suwannik
 # date   : jul 2015
