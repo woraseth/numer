abe8a22e4f7a363645b96d565faa8a957bc37653
woraseth <voraseth@outlook.com>
2015-08-24 19:45:13 +0700

    change while -> for

--- euler_ivp.py
+++ euler_ivp.py
@@ -12,14 +12,14 @@ def euler(f, x0, y0, h, x):
   -------  
   float : solution at x
   """
-  while x0 < x:
+  for i in range(int((x-x0)/h)):
     y0 += f(x0, y0) * h
     x0 += h
   return y0
 
 if __name__ == '__main__':
-  f = lambda x, y : (y**2 - x**2) / 5  # from http://howellkb.uah.edu//DEtext/Part2/Euler_Method.pdf
-  print(euler(f, 0, 1, 0.5, 3))
+  f = lambda x, y : y - x  # from https://www.math.purdue.edu/academic/files/courses/2010spring/MA26200/1_10.pdf
+  print(euler(f, 0, 0.5, 0.1, 1.0))
   
   #f = lambda x, y : -2*x**3 + 12*x**2 - 20*x + 10
   #print(euler(f, 0, 1, 0.5, 4))
