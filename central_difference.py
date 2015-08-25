f3ca72c23dad788d8689294d15203faaecbda64f
woraseth <voraseth@outlook.com>
2015-08-25 21:25:30 +0700

    Create central_difference.py

---
+++ central_difference.py
@@ -0,0 +1,11 @@
+def central_difference(f, x, h):
+  return (f(x+h) - 2*f(x) + f(x-h)) / h**2
+
+if __name__ == '__main__':
+  from math import e
+  f = lambda x : x**2 * e**(2*x)
+  print(central_difference(f, 0, 0.2))
+  
+# derive using central difference method
+# author : worasait suwannik
+# date   : aug 2015
